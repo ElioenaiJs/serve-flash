4fbc3089367c3b08f50278ae23c758d581b30129
Cantrax <carlos.cantrax303@gmail.com>
2025-06-10 09:53:02 -0600

    login primera fase

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatError } from '@angular/material/form-field';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatLabel } from '@angular/material/form-field';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatCardModule, MatError,MatFormField, MatInput, MatLabel],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  
 }
