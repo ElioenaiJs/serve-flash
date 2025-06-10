8bda9d6646c70b6af823b75f880a1e4aff5c44c9
Cantrax <carlos.cantrax303@gmail.com>
2025-06-10 10:04:41 -0600

    materror solucionado

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -2,14 +2,13 @@ import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { MatError } from '@angular/material/form-field';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatLabel } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
-  imports: [MatButtonModule, MatIconModule, MatCardModule, MatError,MatFormField, MatInput, MatLabel],
+  imports: [MatButtonModule, MatIconModule, MatCardModule,MatFormField, MatInput, MatLabel],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
